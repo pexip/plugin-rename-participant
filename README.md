5785e1f3452362170642ae4a431d87ae8eea9b81
Cameron McGuffie <cameronmcguffie@Camerons-MacBook-Pro.local>
2022-03-31 14:35:37 +1000

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pexip Rename Participant Plugin
+
+For documentation on the plugin, please refer to our docs here https://developer.pex.me/plugins/rename-participant
---
+++ renameParticipant.plugin.js
@@ -0,0 +1,53 @@
+(function () {
+  function load(participants$, conferenceDetails$) {
+    console.log('Rename Participant Plugin', 'Loaded');
+  }
+
+  function renameParticipant(participant) {
+    PEX.pluginAPI
+      .openTemplateDialog({
+        title: 'Rename Participant',
+        body: `<div>
+              <input id="NewOverlaytext" class="pex-text-input" placeholder="Type the new overlay text here" autofocus />
+              <button class="dialog-button buttons green-action-button" style="margin-top: 40px" id="NewOverlaytextButton">rename</button>
+              </div>`,
+      })
+      .then((dialogRef) => {
+        document
+          .getElementById('NewOverlaytextButton')
+          .addEventListener('click', () =>
+            submitChange(
+              document.getElementById('NewOverlaytext').value,
+              participant.uuid,
+              dialogRef
+            )
+          );
+
+        dialogRef.close$.subscribe(() => {});
+      });
+  }
+
+  function submitChange(newName, p, ref) {
+    var pUrl = '/participants/' + p + '/overlaytext';
+
+    window.PEX.pluginAPI.sendRequest(pUrl, { text: newName });
+
+    console.log(
+      'Rename Participant Plugin',
+      `submitChange(${newName}, ${p}, ${ref})`
+    );
+
+    ref.close();
+  }
+
+  function unload() {
+    console.log('Rename Participant Plugin', 'Unloaded');
+  }
+
+  PEX.pluginAPI.registerPlugin({
+    id: 'renameParticipant-plugin-1.1',
+    load: load,
+    unload: unload,
+    renameParticipant: renameParticipant,
+  });
+})();
